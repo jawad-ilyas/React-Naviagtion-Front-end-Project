93d91fbcfa8d4d597806421e3879bab7058fd435
jawad ilyas <mughaljawad12@gmail.com>
2024-04-27 00:42:26 +0500

    basic setup of the app complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import heroSvg from "./assets/svg/hero-model.svg"
+
+function App() {
+  return (
+    <div>
+      <img src={heroSvg} />
+    
+    </div>
+  )
+}
+
+export default App
